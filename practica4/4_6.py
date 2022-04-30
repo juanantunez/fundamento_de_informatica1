8f20f119d291de9c09263551f3d20241cd6e1ae0
juanantunez <102978704+juanantunez@users.noreply.github.com>
2022-04-29 21:57:33 -0300

    atrrrrrrrrrrrrrrrrrr

--- practica4/4_6.py
+++ practica4/4_6.py
@@ -3,6 +3,10 @@
 
 #elimine saltos d linea "\n" en archivo d entrada y guardar eso en el archivo d salida
 
+
+#ACLARACION: en este caso no hice funcion xq no tenia argumento xq sino se complica con los r"archivo no los lee
+#ana dijo q es problema en windows algo asi
+
 #def removerNuevaLinea():
 with open(r"C:\ucema_2do_1ro\fundamentos_info\probaratr.txt", "r") as f, open(r"C:\ucema_2do_1ro\fundamentos_info\probar.txt", "w") as s:  #2 archivos abiertos xq nos pide abrir un archivo y escribirlo en otro
   for line in f: #recorro linea a linea el archivo d entrada
--- practica4/4_9.py
+++ practica4/4_9.py
@@ -1,19 +1,20 @@
 #Ejercicio 9
-#Realizá un programa que lea un archivo y obtenga la frecuencia de cada palabra que hay en el archivo. Recordá que la frecuencia es la relación entre número de veces que aparece la palabra en cuestión con respecto a la cantidad total de palabras.
+#Realizá un programa que lea un archivo y obtenga la frecuencia de cada palabra que hay en el archivo. 
+# Recordá que la frecuencia es la relación entre número de veces que aparece la palabra en cuestión con respecto a la cantidad total de palabras.
 
-def wo(arch):             
-  frecuencias={}                 #abro diccio de frecuencias.    frec=veces q aparece tal/total de todas 
-  with open(arch, "r") as f:    #abre archivo
-    word_list=f.read().split()  #lo lee y le hace split->separa elemento x elemento en una lista
-    for i in word_list:         #i(seria la palabra) en la lista de palabras
-      if i in frecuencias:      #si i ya aparecio en la lista de frecuencias
-        frecuencias[i]+=1       #sumale 1 
-      else:
-        frecuencias[i]=1       #si no existe asignale una clave y ponelo en el valor1
-    for word in frecuencias.keys(): #
-      frecuencias[word]=round(frecuencias[word]/len(word_list), 3) #para cada palabra como clave de diccionario(frecuencias)
-                                                                    #para frecuencia de la palabra como valor le pongo ->frecuencia el nro d una palabra dividido el nro total de todas las palabras
-                                                                    #el 3 hace referencia a los 3 decimales eso se puede poner unicamente si antes hay un round
+#def wo(arch):             
+frecuencias={}                 #abro diccio de frecuencias.    frec=veces q aparece tal/total de todas 
+with open(r"C:\ucema_2do_1ro\fundamentos_info\probaratr.txt", "r") as f:    #abre archivo
+  word_list=f.read().split()  #lo lee y le hace split->separa elemento x elemento en una lista
+  for i in word_list:         #i(seria la palabra) en la lista de palabras
+    if i in frecuencias:      #si i ya aparecio en la lista de frecuencias
+      frecuencias[i]+=1       #sumale 1 
+    else:
+      frecuencias[i]=1       #si no existe asignale una clave y ponelo en el valor1
+  for word in frecuencias.keys(): #
+    frecuencias[word]=round(frecuencias[word]/len(word_list), 3) #para cada palabra como clave de diccionario(frecuencias)
+                                                                  #para frecuencia de la palabra como valor le pongo ->frecuencia el nro d una palabra dividido el nro total de todas las palabras
+                                                                  #el 3 hace referencia a los 3 decimales eso se puede poner unicamente si antes hay un round
   print(frecuencias)          
   
   #aux:creo diccionario con la cant de palabras q aparece ->"para"(clave del diccio), si no existe asignale una clave y ponelo en el valor1
